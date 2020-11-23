6490da1bf1e9bcf54e61bfc03d6274bc388bdd63
AngPan <angelopanniello93@gmail.com>
2020-11-23 18:48:31 +0100

    upd risolto problema bottone

--- scroll.js
+++ scroll.js
@@ -1,26 +1,25 @@
 //FLOATBUTTON
- 
-  
-  var rootElement = document.documentElement
-  
-  
-  window.onscroll = function () { scrollFunction() };
-  function scrollFunction() { 
-    
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-  var scrolled = (winScroll / (height)) * 100;
-  document.getElementById("progBar").style.width = scrolled + "%";
 
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-      // Show button
-      document.getElementById("topBtn").classList.add("showBtn")
-    } else {
-      // Hide button
-      document.getElementById("topBtn").classList.remove("showBtn")
-    }
 
- 
+var rootElement = document.documentElement
 
+
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+
+  // var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  // var height = (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  // var scrolled = (winScroll / (height)) * 100;
+  // document.getElementById("progBar").style.width = scrolled + "%";
+
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    // Show button
+    document.getElementById("topBtn").classList.add("showBtn")
+  } else {
+    // Hide button
+    document.getElementById("topBtn").classList.remove("showBtn")
   }
-  
+
+
+
+}
