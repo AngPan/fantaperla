5c4e5adf7a5db4be7e0b9663d7daf68bdf1d67cd
AngPan <angelopanniello93@gmail.com>
2020-12-30 16:02:25 +0100

    upd

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 
-window.addEventListener('load', e => {
-  new PWAConfApp();
-  registerSW();
-});
+function init() {
+  window.addEventListener('load', e => {
+    new PWAConfApp();
+    registerSW();
+  });
 
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./sw.js');
-    } catch (e) {
-      alert('ServiceWorker registration failed. Sorry about that.');
+  async function registerSW() {
+    if ('serviceWorker' in navigator) {
+      try {
+        await navigator.serviceWorker.register('./sw.js');
+      } catch (e) {
+        alert('ServiceWorker registration failed. Sorry about that.');
+      }
+    } else {
+      document.querySelector('.alert').removeAttribute('hidden');
     }
-  } else {
-    document.querySelector('.alert').removeAttribute('hidden');
   }
+
+  //MODALE
+
+  document.getElementById("dwnld").onclick = function () { hide() };
+
+  function hide() {
+    var y = setTimeout(function () {
+      document.getElementById("dwnld").setAttribute("data-dismiss", "modal");
+    }, 1000);
+    clearTimeout(y)
+  }
+  //---------------------
+
+
+  //MAPPA
+
+  function mapsSelector() {
+    if /* if we're on iOS, open in Apple Maps */
+      ((navigator.platform.indexOf("iPhone") != -1) ||
+      (navigator.platform.indexOf("iPad") != -1) ||
+      (navigator.platform.indexOf("iPod") != -1))
+      // window.open("maps://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=");
+      window.open("maps://maps.google.com/maps?q=Via+Chiabrera+39+Roma");
+    else /* else use Google */
+      window.open("http://maps.google.com/?q=Via+Chiabrera+39+Roma");
+  }
+  //--------------------------
+
+
 }
 
 
@@ -21,31 +52,6 @@ async function registerSW() {
 
 
 
-//MODALE
 
-document.getElementById("dwnld").onclick = function () { hide() };
-
-function hide() {
-  var y = setTimeout(function () {
-    document.getElementById("dwnld").setAttribute("data-dismiss", "modal");
-  }, 1000);
-  clearTimeout(y)
-}
-//---------------------
-
-
-//MAPPA
-
-function mapsSelector() {
-  if /* if we're on iOS, open in Apple Maps */
-    ((navigator.platform.indexOf("iPhone") != -1) ||
-    (navigator.platform.indexOf("iPad") != -1) ||
-    (navigator.platform.indexOf("iPod") != -1))
-    // window.open("maps://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=");
-    window.open("maps://maps.google.com/maps?q=Via+Chiabrera+39+Roma");
-  else /* else use Google */
-    window.open("http://maps.google.com/?q=Via+Chiabrera+39+Roma");
-}
-//--------------------------
 
 
