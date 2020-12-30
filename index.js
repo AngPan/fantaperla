0d990fadf3e24cfc620f46e61462a27ee273726e
AngPan <angelopanniello93@gmail.com>
2020-12-30 16:30:00 +0100

    upd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function registerSW() {
     try {
       await navigator.serviceWorker.register('./sw.js');
     } catch (e) {
-      alert('ServiceWorker registration failed. Sorry about that.');
+      console.log("ServiceWorker registration failed.");
     }
   } else {
     document.querySelector('.alert').removeAttribute('hidden');
@@ -21,44 +21,82 @@ async function registerSW() {
 
 
 
-let deferredPrompt; // Allows to show the install prompt
-// const installButton = document.getElementById("install_button");
-// const installButton = document.getElementById("install_button");
-const installButton = $('#install_button');
+// let deferredPrompt; // Allows to show the install prompt
+// // const installButton = document.getElementById("install_button");
+// // const installButton = document.getElementById("install_button");
+// const installButton = $('#install_button');
 
 
-window.addEventListener("beforeinstallprompt", e => {
-  console.log("beforeinstallprompt fired");
-  // Prevent Chrome 76 and earlier from automatically showing a prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Show the install button
-  // installButton.hidden = false;
-  installButton.addEventListener("click", installApp);
+// window.addEventListener("beforeinstallprompt", e => {
+//   console.log("beforeinstallprompt fired");
+//   // Prevent Chrome 76 and earlier from automatically showing a prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Show the install button
+//   // installButton.hidden = false;
+//   installButton.addEventListener("click", installApp);
+// });
+
+
+// function installApp() {
+//   // Show the prompt
+//   deferredPrompt.prompt();
+//   installButton.disabled = true;
+
+//   // Wait for the user to respond to the prompt
+//   deferredPrompt.userChoice.then(choiceResult => {
+//     if (choiceResult.outcome === "accepted") {
+//       console.log("PWA setup accepted");
+//       // installButton.hidden = true;
+//     } else {
+//       console.log("PWA setup rejected");
+//     }
+//     installButton.disabled = false;
+//     deferredPrompt = null;
+//   });
+// }
+
+
+
+
+window.addEventListener('beforeinstallprompt', function (event) {
+  // not show the default browser install app prompt
+  event.preventDefault();
+
+  // add the banner here or make it visible
+  // …
+
+  // save the event to use it later
+  // (it has the important prompt method and userChoice property)
+  window.promptEvent = event;
 });
 
 
-function installApp() {
-  // Show the prompt
-  deferredPrompt.prompt();
-  installButton.disabled = true;
+document.addEventListener('click', function (event) {
+  if (event.target.matches('.install-butt')) {
+    addToHomeScreen();
+  }
+});
 
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then(choiceResult => {
-    if (choiceResult.outcome === "accepted") {
-      console.log("PWA setup accepted");
-      // installButton.hidden = true;
+function addToHomeScreen() {
+  // show the install app prompt
+  window.promptEvent.prompt();
+
+  // handle the Decline/Accept choice of the user
+  window.promptEvent.userChoice.then(function (choiceResult) {
+    // hide the prompt banner here
+    // …
+
+    if (choiceResult.outcome === 'accepted') {
+      console.info('mm User accepted the A2HS prompt');
     } else {
-      console.log("PWA setup rejected");
+      console.info('mm User dismissed the A2HS prompt');
     }
-    installButton.disabled = false;
-    deferredPrompt = null;
+
+    window.promptEvent = null;
   });
 }
-
-
-
 
 
 
