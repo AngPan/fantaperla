27a24db39458f2830b27d23a5c9b79c9d06805cb
AngPan <angelopanniello93@gmail.com>
2021-01-21 12:12:18 +0100

    upd

---
+++ index2.js
@@ -0,0 +1,25 @@
+/* 
+    Short cuts for document querySelector and querySelectorAll
+    which can handle being used as template tags $`main` or $$`div`
+    
+    Also if the first character is < then create a fragment instead
+*/
+const $ = (...s) => {
+    const str = Array.isArray(s[0]) ? String.raw(...s) : s[0];
+    return str[0] === '<' ? document.createRange().createContextualFragment(str) : document.querySelector(str);
+}
+
+const $$ = (...s) => {
+    const str = Array.isArray(s[0]) ? String.raw(...s) : s[0];
+    return Array.from(str[0] === '<' ? $(str).children : document.querySelectorAll(str));
+}
+
+/* Function for fetching a document fragment and replacing an element with it */
+function replace(el, url) {
+    return fetch(url)
+        .then(r => r.text())
+        .then(html => {
+            el.parentNode.insertBefore($(html), el);
+            el.remove();
+        });
+}
