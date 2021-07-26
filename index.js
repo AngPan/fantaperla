c35a0ff230d6d87b67748fdf9e98061fded00f78
AngPan <angelopanniello93@gmail.com>
2021-07-26 20:42:09 +0200

    UPDATE TO FANTAPERLA+

--- index.js
+++ index.js
@@ -1,92 +1,114 @@
+const $ = (...s) => {
+    const str = Array.isArray(s[0]) ? String.raw(...s) : s[0];
+    return str[0] === '<' ? document.createRange().createContextualFragment(str) : document.querySelector(str);
+}
 
+const $$ = (...s) => {
+    const str = Array.isArray(s[0]) ? String.raw(...s) : s[0];
+    return Array.from(str[0] === '<' ? $(str).children : document.querySelectorAll(str));
+}
+
+/* Function for fetching a document fragment and replacing an element with it */
+function replace(el, url) {
+    return fetch(url)
+        .then(r => r.text())
+        .then(html => {
+            el.parentNode.insertBefore($(html), el);
+            el.remove();
+        });
+}
+
+const iO = new IntersectionObserver(entries => entries.forEach(entry => {
+    const hash = '#' + entry.target.id;
+    const navEl = $ `[href="${hash}"]`;
+    if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
+        navEl.classList.add('focus');
+        updateHistory(hash);
+    } else {
+        navEl.classList.remove('focus');
+    }
+}), {
+    root: $ `main`,
+    threshold: 0.5
+});
+
+
+
+window.addEventListener('DOMContentLoaded', () => $$ `article`.map(a => iO.observe(a)));
+window.addEventListener('hashchange', function(e) {
+    const articleToShow = $(window.location.hash || '#' + $ `article`.id);
+    articleToShow.scrollIntoView();
+    e.preventDefault();
+}, false);
+
+/* Update the window URL on swipe, this is throttled so that the history doesn't get filled with useless entries*/
+function updateHistory(hash) {
+    clearTimeout(updateHistory.timeout);
+    updateHistory.timeout = setTimeout(function() {
+        if (window.location.hash !== hash) {
+            if (location.hash !== '') {
+                history.pushState({}, window.title, hash);
+            } else {
+
+                // On first page load update the URL in place
+                history.replaceState({}, window.title, hash);
+            }
+        }
+    }, 1000);
+}
 
 window.addEventListener('load', e => {
-  // new PWAConfApp();
-  registerSW();
+    // new PWAConfApp();
+    registerSW();
 });
 
 async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./sw.js');
-    } catch (e) {
-      console.log("ServiceWorker registration failed.");
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+            console.log("ServiceWorker registration failed.");
+        }
+    } else {
+        document.querySelector('.alert').removeAttribute('hidden');
     }
-  } else {
-    document.querySelector('.alert').removeAttribute('hidden');
-  }
 }
 
+window.addEventListener('beforeinstallprompt', function(event) {
+    // not show the default browser install app prompt
+    event.preventDefault();
+    // add the banner here or make it visible
+    // …
+    // console.log("App non installata");
+    // $('#install_button').css('display', 'flex');
 
-window.addEventListener('beforeinstallprompt', function (event) {
-  // not show the default browser install app prompt
-  event.preventDefault();
-  // add the banner here or make it visible
-  // …
-  // console.log("App non installata");
-  // $('#install_button').css('display', 'flex');
-
-  // save the event to use it later
-  // (it has the important prompt method and userChoice property)
-  window.promptEvent = event;
+    // save the event to use it later
+    // (it has the important prompt method and userChoice property)
+    window.promptEvent = event;
 });
 
 
-document.addEventListener('click', function (event) {
-  if (event.target.matches('.install-trigger')) {
-    addToHomeScreen();
-  }
+document.addEventListener('click', function(event) {
+    if (event.target.matches('.install-trigger')) {
+        addToHomeScreen();
+    }
 });
 
 function addToHomeScreen() {
-  // show the install app prompt
-  window.promptEvent.prompt();
+    // show the install app prompt
+    window.promptEvent.prompt();
 
-  // handle the Decline/Accept choice of the user
-  window.promptEvent.userChoice.then(function (choiceResult) {
-    // hide the prompt banner here
-    // …
+    // handle the Decline/Accept choice of the user
+    window.promptEvent.userChoice.then(function(choiceResult) {
+        // hide the prompt banner here
+        // …
 
-    if (choiceResult.outcome === 'accepted') {
-      console.info('mm User accepted the A2HS prompt');
-    } else {
-      console.info('mm User dismissed the A2HS prompt');
-    }
+        if (choiceResult.outcome === 'accepted') {
+            console.info('mm User accepted the A2HS prompt');
+        } else {
+            console.info('mm User dismissed the A2HS prompt');
+        }
 
-    window.promptEvent = null;
-  });
+        window.promptEvent = null;
+    });
 }
-
-
-
-
-
-
-
-//MODALE
-
-document.getElementById("dwnld").onclick = function () { hide() };
-
-function hide() {
-  var y = setTimeout(function () {
-    document.getElementById("dwnld").setAttribute("data-dismiss", "modal");
-  }, 1000);
-  clearTimeout(y)
-}
-//---------------------
-
-
-//MAPPA
-
-function mapsSelector() {
-  if /* if we're on iOS, open in Apple Maps */
-    ((navigator.platform.indexOf("iPhone") != -1) ||
-    (navigator.platform.indexOf("iPad") != -1) ||
-    (navigator.platform.indexOf("iPod") != -1))
-    // window.open("maps://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=");
-    window.open("maps://maps.google.com/maps?q=Via+Chiabrera+39+Roma");
-  else /* else use Google */
-    window.open("http://maps.google.com/?q=Via+Chiabrera+39+Roma");
-}
-  //--------------------------
-
