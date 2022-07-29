944c7413f20e08af27875da9d3b03963f0ce65c9
AngPan <angelopanniello93@gmail.com>
2022-07-29 18:58:51 +0200

    upd

--- install.js
+++ install.js
@@ -19,3 +19,23 @@ export class Install {
         }
     }
 }
+
+let defferedPrompt;
+const addbtn = document.querySelector('.btn');
+
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault();
+    defferedPrompt = event
+    addbtn.style.display = 'block';
+});
+
+addbtn.addEventListener('click', event => {
+    defferedPrompt.prompt();
+
+    defferedPrompt.userChoice.then(choice => {
+        if (choice.outcome === 'accepted') {
+            console.log('user accepted the prompt')
+        }
+        defferedPrompt = null;
+    })
+})
