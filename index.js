51ca98ab07a6d9dd5f68014f980b74aac701c723
AngPan <angelopanniello93@gmail.com>
2022-07-29 13:52:30 +0200

    upd

--- index.js
+++ index.js
@@ -27,3 +27,14 @@ window.addEventListener('beforeinstallprompt', (e) => {
         });
     });
 });
+
+
+/* Function for fetching a document fragment and replacing an element with it */
+function replace(el, url) {
+    return fetch(url)
+        .then(r => r.text())
+        .then(html => {
+            el.parentNode.insertBefore($(html), el);
+            el.remove();
+        });
+}
